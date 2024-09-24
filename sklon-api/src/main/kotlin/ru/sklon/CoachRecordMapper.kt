3596318fe58ad234be39b8d091781fe9d38f2c90
Абаев Евгений <abaev@flydrone.ru>
2024-09-24 05:00:28 +0300

    тест пайплайна на другой ветке

--- sklon-api/src/main/kotlin/ru/sklon/CoachRecordMapper.kt
+++ sklon-api/src/main/kotlin/ru/sklon/CoachRecordMapper.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 import ru.sklon.jooq.domain.sklon_db.tables.Users.USERS
 
 /**
- * iuyf i
+ * iuyf iljhvklhj
  * @author Abaev Evgeniy
  */
 @Component
